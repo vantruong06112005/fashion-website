41537428f059c37ad00fc5820d7bdd8bdeea8c5a
Nguyen Truong <23653651.truong@student.iuh.edu.vn>
2026-08-16 21:44:13 +0700

    first commit

---
+++ src/main/java/iuh/fit/fashionwebsite/FashionWebsiteApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.fashionwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionWebsiteApplication.class, args);
+    }
+
+}
